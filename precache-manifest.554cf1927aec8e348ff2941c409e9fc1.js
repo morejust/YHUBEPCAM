3516845af1fe47277cb63de83bd1fcba78a019f9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-08 13:56:48 +0000

    Deploy morejust/YHUBEPCAM-KASSA to github.com/morejust/YHUBEPCAM-KASSA.git:gh-pages

---
+++ precache-manifest.554cf1927aec8e348ff2941c409e9fc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3ffee8262ed273ff71428f69902d001",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f1765dfaf6f5876dfa2",
+    "url": "/static/css/main.b494dae3.chunk.css"
+  },
+  {
+    "revision": "e72ff7203a7fe614cc6d",
+    "url": "/static/js/2.91e3e36a.chunk.js"
+  },
+  {
+    "revision": "5f1765dfaf6f5876dfa2",
+    "url": "/static/js/main.6c03f45b.chunk.js"
+  },
+  {
+    "revision": "3f0520d5c82234d8e788",
+    "url": "/static/js/runtime~main.9b509fb9.js"
+  },
+  {
+    "revision": "8624eb1e8f5d807b40e1c990d418b2de",
+    "url": "/static/media/logo.8624eb1e.svg"
+  }
+]);
