05ba11c78f6e68bb243f1da6cc75d18c253c4ce1
Aleksey Bykhun <theisease@gmail.com>
2019-09-08 14:50:28 +0300

    open links in new tab

--- src/components/TxLink.js
+++ src/components/TxLink.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { shortHash } from '../utils'
 
 export const TxLink = props => (
-  <a href={`https://wavesexplorer.com/testnet/tx/${props.txId}`}>
+  <a href={`https://wavesexplorer.com/testnet/tx/${props.txId}`} target="_blank" rel="noopener">
     {shortHash(props.txId)}
   </a>
 )
